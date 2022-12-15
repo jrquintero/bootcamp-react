ea4ce5c538c059c7b1d71f69dd037f370085aec5
Johan Quintero <jrquintero@gmail.com>
2022-12-14 22:59:09 -0400

    Primeras pruebas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Mensaje from './Mensaje';
 
-function App() {
+
+const Descripcion = () => {
+  return <span>Esto es una prueba de JSX</span>
+}
+
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mensaje color='red' message='Esto es un parametro'/>
+      <p>Corriendo el {now.toString()}</p>
+      <Descripcion />
+      <p>
+        {a} mas {b} es {a + b}
+      </p>
+      <Mensaje color='blue' message='Esto es un parametro'/>
+      <Descripcion />
     </div>
   );
 }
+
 
 export default App;
